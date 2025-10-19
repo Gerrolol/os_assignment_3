8b8c8993ffb4bc257e533d6da7a49119b4e3142d
UniAccLeo <a1853245@adelaide.edu.au>
2025-10-19 15:23:10 +1030

    fixed

--- comp2002-os-mergesort/mergesort.c
+++ comp2002-os-mergesort/mergesort.c
@@ -19,18 +19,18 @@ void my_mergesort(int left, int right) {}
 /* this function will be called by the testing program. */
 void* parallel_mergesort(void* arg) {
   struct argument* args = (struct argument*)arg;
-  if (args->right - args->left <= 0) return;
+  if (args->right - args->left <= 0) return NULL;
   if (args->level == cutoff)
     my_mergesort(args->left, args->right);
   else {
     int center = (args->left + args->right) / 2;
-    pthread_t* loThread;
-    pthread_t* hiThread;
+    pthread_t loThread;
+    pthread_t hiThread;
     struct argument* loArgs =
         buildArgs(args->left, center - 1, args->level + 1);
     struct argument* hiArgs = buildArgs(center, args->right, args->level + 1);
-    pthread_create(loThread, NULL, parallel_mergesort, loArgs);
-    pthread_create(hiThread, NULL, parallel_mergesort, hiArgs);
+    pthread_create(&loThread, NULL, parallel_mergesort, loArgs);
+    pthread_create(&hiThread, NULL, parallel_mergesort, hiArgs);
     pthread_join(loThread, NULL);
     pthread_join(hiThread, NULL);
     merge(args->left, center - 1, center, args->right);
